ad407ebca7de50f638d286fb3996d1e88a495deb
Thinh Phan <phantyson@gmail.com>
2026-02-17 18:22:46 -0500

    ADd GraphQL schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,1 +1,45 @@
-import { gql } from 'graphql-tag';
+import gql from "graphql-tag";
+
+const movieSchema = gql`
+
+  type Movie {
+    id: ID!
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+    movie_age: Int
+  }
+
+  input MovieInput {
+    name: String!
+    director_name: String!
+    production_house: String!
+    release_date: String!
+    rating: Float!
+  }
+
+  input MovieUpdateInput {
+    name: String
+    director_name: String
+    production_house: String
+    release_date: String
+    rating: Float
+  }
+
+  type Query {
+    getAllMovies: [Movie!]!
+    getMovieById(id: ID!): Movie
+    getMoviesByDirector(director_name: String!): [Movie!]!
+  }
+
+  type Mutation {
+    addMovie(movie: MovieInput!): Movie!
+    updateMovie(id: ID!, movie: MovieUpdateInput!): Movie
+    deleteMovieById(id: ID!): Boolean!
+  }
+
+`;
+
+export default movieSchema;
